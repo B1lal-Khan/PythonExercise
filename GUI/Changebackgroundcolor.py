3a25cbc745d0868b70ae2ff08722da6ff4c64c5c
Bilal Khan <21299@students.mrgs.school.nz>
2024-03-04 10:49:35 +1300

    Made a quiz

--- GUI/Changebackgroundcolor.py
+++ GUI/Changebackgroundcolor.py
@@ -1,7 +1,13 @@
-from tkinter import *
+import tkinter as tk
 
 def color_change(color):
     main['bg'] = color
 
 main = tk.Tk()
 main.title("Colour changer")
+
+for color in ["red", "blue"]:
+    button = tk.Button(main_window, text=color,capitalise(), command=lambda c=color:
+    button.pack(pady=10)
+    
+main_window.mainloop()
