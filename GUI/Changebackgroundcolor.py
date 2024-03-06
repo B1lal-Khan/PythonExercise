c99507b937b4432ca05395abbd034fd112d0ce82
Bilal Khan <21299@students.mrgs.school.nz>
2024-03-06 14:39:39 +1300

    Started working on a student thing

--- GUI/Changebackgroundcolor.py
+++ GUI/Changebackgroundcolor.py
@@ -3,14 +3,14 @@ import tkinter as tk
 def change_color(color):
     main_window['bg'] = color
 
-# Create the main window
+# Creating the main window
 main_window = tk.Tk()
 main_window.title("Color Changer")
 
-# Create buttons and set their command to the change_color function
+# Setting the buttons commands to the colours
 for color in ["red", "blue"]:
     button = tk.Button(main_window, text=color.capitalize(), command=lambda c=color: change_color(c))
     button.pack(pady=10)
 
-# Run the Tkinter event loop
+# Running the Tkinter event loop
 main_window.mainloop()
---
+++ GUI/student.py
@@ -0,0 +1,23 @@
+from typing import Self
+
+
+class student():
+    
+    
+    def__init__(self): # type: ignore
+    Self.first_name = "Bilal"
+        
+    def display_name(self):
+        print(self.first_name)
+        
+    def set_grade(self, grade):
+        self.grade = grade
+        
+    def display_name(self):
+        print(self.first_name)
+
+twelve_csc = []
+
+twelve_csc.append(student("Bilal"))
+
+twelve_csc[0].display_name()
